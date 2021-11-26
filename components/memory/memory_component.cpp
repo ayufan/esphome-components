5d0042f9a64534195a5ad4fe7e64f8b9f3fbaf17
Kamil Trzciński <ayufan@ayufan.eu>
2021-11-26 18:42:18 +0100

    Fix `memory` component

--- components/memory/memory_component.cpp
+++ components/memory/memory_component.cpp
@@ -1,8 +1,14 @@
 #include "memory_component.h"
 #include "esphome/core/log.h"
-#include "esphome/core/helpers.h"
-#include "esphome/core/defines.h"
-#include "esphome/core/version.h"
+
+#ifdef USE_ESP_IDF
+#include <esp_heap_caps.h>
+#include <esp_system.h>
+#endif
+
+#ifdef USE_ARDUINO
+#include <Esp.h>
+#endif
 
 namespace esphome {
 namespace debug {
@@ -10,7 +16,12 @@ namespace debug {
 static const char *TAG = "memory";
 
 void MemoryComponent::update() {
-  uint32_t free_heap = ESP.getFreeHeap();
+#ifdef USE_ARDUINO
+  uint32_t free_heap = ESP.getFreeHeap();  // NOLINT(readability-static-accessed-through-instance)
+#elif defined(USE_ESP_IDF)
+  uint32_t free_heap = heap_caps_get_free_size(MALLOC_CAP_INTERNAL);
+#endif
+
   ESP_LOGD(TAG, "Free Heap Size: %u bytes", free_heap);
 }
 
